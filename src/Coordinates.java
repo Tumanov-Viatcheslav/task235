67fd2993ddf1cb4ffe0272c6fbe9a99843ebdb04
Tumanov <tslavich@bk.ru>
2024-09-19 14:06:16 +0300

    -Another hashCode declaration

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,16 +1,32 @@
+import java.util.Objects;
+
 public class Coordinates {
     int x, y;
 
-    @Override public int hashCode() {
-        return (x * 31) ^ y;
+//    @Override public int hashCode() {
+//        return (x * 31) ^ y;
+//    }
+//
+//    @Override public boolean equals(Object o) {
+//        if (o instanceof Coordinates) {
+//            Coordinates p = (Coordinates) o;
+//            return (p.x == x) && (p.y == y);
+//        }
+//        return false;
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
     }
 
-    @Override public boolean equals(Object o) {
-        if (o instanceof Coordinates) {
-            Coordinates p = (Coordinates) o;
-            return (p.x == x) && (p.y == y);
-        }
-        return false;
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
     public Coordinates copy() {
